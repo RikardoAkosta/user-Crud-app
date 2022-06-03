2289f2d7625a89f7c7f71e9029612d6853bc4ca0
Rikardo Akosta <richy.andres.acosta@gmail.com>
2022-06-03 11:41:48 +0200

    se hace taller 4 entrega cambio de diseño segun ajustes figma

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import './App.css';
+import UserList from './components/UserList';
+import UsersForm from './components/UsersForm';
 
 function App() {
+
+  const [users,setUsers]=useState([]);
+  const[selectUser,setSelectUser]=useState(null);
+
+  useEffect(()=>{
+    axios.get('https://users-crud1.herokuapp.com/users/')
+      .then(res=>setUsers(res.data))
+  },[])
+  
+  const getUsers = ()=>{
+    axios.get('https://users-crud1.herokuapp.com/users/')
+      .then(res=>setUsers(res.data))
+      
+  }
+
+  const editUser = user =>{
+    setSelectUser(user)
+    setShowModal(true)
+
+  }
+
+  const deselectUser = ()=>{
+    setSelectUser(null)
+  }
+
+  const deleteUser = user =>{
+    axios.delete(`https://users-crud1.herokuapp.com/users/${user.id}/`)
+    .then(()=>getUsers())
+    alert(`${user.first_name} ${user.last_name} se ha Eliminado`)
+    
+  }
+  
+  const [showModal,setShowModal]=useState(false);
+
+  const closeModal = ()=> setShowModal(false)
+
+
+  
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      { showModal && <UsersForm
+        getUsers={getUsers}
+        selectUser={selectUser}
+        deselectUser={deselectUser}
+        closeModal={closeModal}
+      />}
+      <div className='addUser-button-container'>
+        <h2 className='title'>Users List</h2>
+        <button onClick={()=>setShowModal(true)} className="addUser-button">Agregar Usuario</button>
+      </div>
+
+      <UserList 
+        users={users}
+        editUser={editUser}
+        deleteUser={deleteUser}
+      />
+
+      
     </div>
   );
 }
